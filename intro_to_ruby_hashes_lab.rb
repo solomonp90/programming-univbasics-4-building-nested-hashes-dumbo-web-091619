cf1fd71bf8c872dd9743f796d0865af808d713f2
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-29 16:21:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 	  :railroads => {
 	 		:pieces => 4,
 	 			:rent_in_dollars => {:key1 => {},:key2 => {},:key3 => {},:key4 => {}},
-				:names => {}
+				:names => {:key1 => {},:key2 => {},:key3 => {},:key4 => {}},
 
 	 			}}
 end

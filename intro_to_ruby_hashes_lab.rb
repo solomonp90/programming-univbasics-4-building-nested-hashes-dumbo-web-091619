7b905e065af3c319dac7cddfa90bae2140bfb38f
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-29 17:57:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,14 @@ rr_value = 100
 					:three_pieces_owned=>100,
 					:four_pieces_owned=>200},
 				:names=>{
-					:reading_railroad=>{"mortgage_value"=>"$100"},:pennsylvania_railroad=>{"mortgage_value"=>"$200"},:b_and_o_railroad=>{"mortgage_value"=>"$400"},:shortline_railroad=>{"mortgage_value"=>"$800"}},
+					:reading_railroad=>{
+						    "mortgage_value"=>"$100"},
+					:pennsylvania_railroad=>{
+						    "mortgage_value"=>"$200"},
+					:b_and_o_railroad=>{
+								"mortgage_value"=>"$400"},
+					:shortline_railroad=>{
+								"mortgage_value"=>"$800"}},
 
 			}}
 

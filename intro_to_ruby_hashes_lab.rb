10e835341abd339fa66f550acbadb437a7fdf2f3
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-29 17:01:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,10 @@ def monopoly_with_fourth_tier
 					:three_pieces_owned=>100,
 					:four_pieces_owned=>200},
 				:names=>{
-					:reading_railroad=>{:mortgage=>"$100"},:pennsylvania_railroad=>{},:b_and_o_railroad=>{},:shortline_railroad=>{}},
+					:reading_railroad=>{:mortgage_value=>"$100"},
+					:pennsylvania_railroad=>{:mortgage_value=>},
+					:b_and_o_railroad=>{:mortgage_value=>},
+					:shortline_railroad=>{:mortgage_value=>}},
 
 				}}
 end
